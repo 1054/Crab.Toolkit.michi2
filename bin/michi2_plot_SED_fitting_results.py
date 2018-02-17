3b6afa34d45c41159416caba768dc994279e44cf
1054 <liudz1054@gmail.com>
2018-02-18 00:21:00 +0100

    Updates

--- bin/michi2_plot_SED_fitting_results.py
+++ bin/michi2_plot_SED_fitting_results.py
@@ -210,19 +210,23 @@ def analyze_chisq_distribution(param_dict, verbose = 1, Plot_engine = None):
                                 overplot = True, linestyle = 'dashed')
         # 
         # Plot histogram (right panel)
-        Plot_engine.plot_hist(param_bin_x, 1/numpy.array(param_bin_y), width = param_bin_step*1.5, align = 'edge', overplot = False, 
-                                xtitle = param_dict['Par_name'], ytitle = '$1/\chi^2$', useTex = True, 
-                                xrange = xrange, yrange = yrange, xlog = xlog, ylog = ylog)
-        # 
-        # Plot Cut_chi2 line
-        Plot_engine.plot_line(xrange[0], 1/(param_stats['threshold_chisq']), xrange[1], 1/(param_stats['threshold_chisq']), overplot = True, linestyle = 'dashed')
-        Plot_engine.plot_text(xrange[1], yrange[1]-0.02*(yrange[1]-yrange[0]), ' (zoomed) ', NormalizedCoordinate=False, overplot=True, horizontalalignment='right', verticalalignment='top')
-        # 
-        # Plot Cut_chi2 line (2p = 2.3)
-        Plot_engine.plot_line(param_stats_2p['xrange'][0], 1/(param_stats_2p['threshold_chisq']), 
-                                param_stats_2p['xrange'][1], 1/(param_stats_2p['threshold_chisq']), 
-                                overplot = True, color='#1e90ff', linestyle = 'dotted')
-                                # color: http://www.color-hex.com/color/1e90ff
+        if param_stats_2p['valid']:
+            Plot_engine.plot_hist(param_bin_x, 1/numpy.array(param_bin_y), width = param_bin_step*1.5, align = 'edge', overplot = False, 
+                                    xtitle = param_dict['Par_name'], ytitle = '$1/\chi^2$', useTex = True, 
+                                    xrange = xrange, yrange = yrange, xlog = xlog, ylog = ylog)
+            # 
+            # Plot Cut_chi2 line
+            Plot_engine.plot_line(xrange[0], 1/(param_stats['threshold_chisq']), xrange[1], 1/(param_stats['threshold_chisq']), overplot = True, linestyle = 'dashed')
+            Plot_engine.plot_text(xrange[1], yrange[1]-0.02*(yrange[1]-yrange[0]), ' (zoomed) ', NormalizedCoordinate=False, overplot=True, horizontalalignment='right', verticalalignment='top')
+            # 
+            # Plot Cut_chi2 line (2p = 2.3)
+            Plot_engine.plot_line(param_stats_2p['xrange'][0], 1/(param_stats_2p['threshold_chisq']), 
+                                    param_stats_2p['xrange'][1], 1/(param_stats_2p['threshold_chisq']), 
+                                    overplot = True, color='#1e90ff', linestyle = 'dotted')
+                                    # color: http://www.color-hex.com/color/1e90ff
+        else:
+            Plot_engine.plot_text(1.0-0.02, 1.00-0.02, ' (zoomed) ', NormalizedCoordinate=True, overplot=False, horizontalalignment='right', verticalalignment='top')
+            Plot_engine.plot_text(0.5, 0.5, ' (No valid data) ', NormalizedCoordinate=True, overplot=True, horizontalalignment='center', verticalalignment='center')
         # 
     else:
         print('Error! analyze_chisq_distribution() got unaccepted inputs!')
