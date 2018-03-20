8578bca4ee36ad178e8c2406a01a16eaefb7cdf2
1054 <liudz1054@gmail.com>
2018-03-20 03:56:30 +0100

    Updates

--- bin/bin_magphys/magphys_highz/get_infrared_colors_highz.f
+++ bin/bin_magphys/magphys_highz/get_infrared_colors_highz.f
@@ -201,7 +201,7 @@ c       ------------------------------------------------------------------------
         implicit none
         integer nf
         integer i,inw,icall
-	integer ifilt(200) ! dzliu modified
+	integer ifilt(nf),wfilt(nf) ! dzliu modified, now support up to 200 filters ! dzliu added argument ",wfilt(nf)"
 	real x(inw),yd(inw),fx(nf)
 	real z,f_mean,dl,mag(nf)
 	data icall/0/
--- bin/bin_magphys/magphys_highz/get_optic_colors_highz.f
+++ bin/bin_magphys/magphys_highz/get_optic_colors_highz.f
@@ -241,7 +241,7 @@ c       ------------------------------------------------------------------------
         integer nf,n,id,nter
         parameter(n=100,nter=2)
         integer i,inw,icall,iread,jread,ireset
-	integer ifilt(nf),lambda_rest(nf) ! dzliu modified, now support up to 200 filters ! dzliu added argument ",lambda_rest(nf)"
+	integer ifilt(nf),wfilt(nf) ! dzliu modified, now support up to 200 filters ! dzliu added argument ",wfilt(nf)"
 	real x(inw),ys(inw),y0s(inw),fx(nf)
 	real z,f_mean,dl,tauIGM,factor
         real*8 mag(nf)
