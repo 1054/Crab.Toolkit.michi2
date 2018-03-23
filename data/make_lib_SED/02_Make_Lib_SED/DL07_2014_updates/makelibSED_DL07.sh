7bda060da8d585d54b420478373d49185e753f83
1054 <liudz1054@gmail.com>
2018-03-23 12:41:15 +0100

    Updates

--- data/make_lib_SED/02_Make_Lib_SED/DL07_2014_updates/makelibSED_DL07.sh
+++ data/make_lib_SED/02_Make_Lib_SED/DL07_2014_updates/makelibSED_DL07.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 
 
-echo "macro read makelibSED_DL07.sm makelibSED_DL07" | sm | tee makelibSED_DL07.log
+echo "macro read makelibSED_DL07_spec_2p0.sm makelibSED_DL07" | sm | tee makelibSED_DL07_spec_2p0.log
 
 
