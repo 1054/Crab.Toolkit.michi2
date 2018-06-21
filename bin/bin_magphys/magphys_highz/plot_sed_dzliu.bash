c47e5c9a0f08fe78794aec12bf461a4ab1078594
1054 <liudz1054@gmail.com>
2018-06-21 12:50:46 +0200

    Updates

---
+++ bin/bin_magphys/magphys_highz/plot_sed_dzliu.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 
+
+export IDL_PATH="$IDL_PATH:$HOME/Cloud/Github/Crab.Toolkit.michi2/bin/bin_magphys/magphys_highz"
+export USER_OBS="magphys_input_fluxes.dat"
+export USER_FILTERS="magphys_input_filters.dat"
+export magphys="$HOME/Softwares/magphys/magphys_highz"
+
+idl -e plot_sed -args "best-fit_SED"
