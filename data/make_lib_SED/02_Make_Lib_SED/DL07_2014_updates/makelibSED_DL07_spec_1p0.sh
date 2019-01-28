5671615502153dd72108007dd5329aadd75f83a5
1054 <liudz1054@gmail.com>
2019-01-28 13:27:57 +0100

    Updates

---
+++ data/make_lib_SED/02_Make_Lib_SED/DL07_2014_updates/makelibSED_DL07_spec_1p0.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 
+
+echo "macro read makelibSED_DL07_spec_1p0.sm makelibSED_DL07" | sm | tee makelibSED_DL07_spec_1p0.log
+
+#7z a -tzip lib.DL07UPD2010.DuoCom.SED.zip lib.DL07.2010.03.18.spec.2.0.HiExCom.SED lib.DL07.2010.03.18.spec.2.0.LoExCom.SED
+
+#rm DL07spec
+
